968605442a087ca26427036f7fde05c7d4e6de9d
Dagur Valberg Johannsson <dagurval@pvv.ntnu.no>
2019-03-28 10:39:42 +0100

    [qa] Add electrum_basic test

--- qa/pull-tester/rpc-tests.py
+++ qa/pull-tester/rpc-tests.py
@@ -240,7 +240,8 @@ testScripts = [ RpcTest(t) for t in [
     'xversion',
     'sighashmatch',
     'getlogcategories',
-    'getrawtransaction'
+    'getrawtransaction',
+    Disabled('electrum_basics', "Needs to be skipped if electrs is not built")
 ] ]
 
 testScriptsExt = [ RpcTest(t) for t in [
